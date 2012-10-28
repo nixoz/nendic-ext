526063389534591471fa248d2d032e831934eda3
Ohgyun Ahn <ohgyun@gmail.com>
2012-10-28 21:34:29 +0900

    add frameObserver

--- src/js/cscript/cscript_main.js
+++ src/js/cscript/cscript_main.js
@@ -10,21 +10,38 @@ require([
   'common/pubsub',
   'cscript/frameObserver'
   
-], function ($, pubsub) {
+], function ($, pubsub, frameObserver) {
   
   console.log('cscri3pt_main');
-  
-  pubsub.sub('word-searched', function (data) {
-    console.log('단어 검색이 완료됐따!', data);
-  });
-  
-  
-  pubsub.sub('user-actioned', function (data) {
-    
-    require([], function () {
-      // 필요한 코드는 실제로 사용자 액션이 발생했을 때 로드한다.  
+ 
+  // 프레임 관찰자 
+  //--------------
+  // 프레임 관찰자에 핸들러를 등록한다.
+  frameObserver.onObserve(function (frameId) {
+    console.log('frameId: ', frameId);
+    pubsub.pub('frame-observed', {
+      frameId: frameId
     });
-    
+  });
+
+  // 프레임 리파지터리가 초기화된 경우,
+  // 현재 프레임 정보를 수집해 전송한다. 
+  pubsub.sub('frameRepos-emptied', function () {
+    var frameInfo = frameObserver.getInfo();
+    pubsub.pub('frame-infoCollected', frameInfo);
+  });
+
+  // 활성화할 프레임을 찾았다는 정보를 받은 경우,
+  // 해당 정보로 프레임을 활성화/비활성화한다.
+  pubsub.sub('frameRepos-activatableFrameFound', function (data) {
+    frameObserver.activate(data.frameId);  
+  });
+
+  // 사전 뷰어
+  //----------
+  // 단어 검색이 완료된 경우
+  pubsub.sub('word-searched', function (data) {
+    console.log('검색 완료됨. 뷰어를 띄운다.', data);
   });
   
   
--- src/js/cscript/frameObserver.js
+++ src/js/cscript/frameObserver.js
@@ -1,29 +1,93 @@
 /**
  * 프레임 관찰자 모듈
  * 
+ * 한 페이지에는 여러 프레임이 존재할 수 있다.
+ * 페이지가 프레임으로 나뉘어져 있을 수도 있고,
+ * 페이지 내에 광고가 포함되어 있을 수도 있다.
+ *
+ * 영어사전은 페이지 내의 DOM에 사전 엘리먼트를
+ * 직접 삽입하는 방식을 사용하고 있으며,
+ * 이 때문에, 모든 프레임에 컨텐트 스크립트를 추가해야 한다.
+ *
+ * 그리고, 모든 프레임에 사전을 보여주는 것은 적합하지 않으므로,
+ * 페이지 내 프레임을 탐색해 사전을 띄우기에
+ * 가장 적합한 프레임을 찾아내야 한다.
+ * 
+ * 이 모듈에서는 사용자가 프레임에 마우스를 클릭하는 것을 관찰하고,
+ * 프레임에 고유 아이디를 할당하고 사이즈 정보를 알려준다. 
+ *
  * @module frameObserver
  * @author ohgyun@gmail.com
  * @version 2012.10.27
  */
-require([
+define([
   
   'jquery',
   'common/util'
   
 ], function ($, util) {
   
-  var _guid = util.guid();
+  var _id = util.guid(),
+    _handlers = [],
+    _activated = false,
+    _$doc = $(document);
   
   
+  // 프레임에 움직임이 관찰되었을 때 핸들러를 호출한다.
+  // 사용자가 마우스를 클릭한 시점이다.
+  $(document).mousedown(function (e) {
+    _handlers.forEach(function (handler) {
+      handler(_id);
+    });
+  });  
+
+
   /**
-   * 프레임을 관찰할 이벤트를 등록한다.
+   * 프레임 정보를 가져온다.
    */
-  function registerFrameActivateEvent() {
-    $(document).mousedown(function (e) {
-      console.log('e')
-    });  
+  function getInfo() {
+    return {
+      id: _id,
+      width: _$doc.width(),
+      height: _$doc.height()
+    };
   }
   
-  
-  
+  return {
+    /**
+     * 프레임 내에 액션이 관찰되었을 때 호출할
+     * 핸들러를 등록한다.
+     * @param {function (id)} handler
+     */
+    onObserve: function (handler) {
+      _handlers.push(handler);
+    },
+
+    /**
+     * 현재 프레임 정보를 가져온다.
+     * @return {object} info
+     *    info.id 프레임 고유아이디
+     *    info.width 프레임 너비
+     *    info.height 프레임 높이
+     */
+    getInfo: function () {
+      return getInfo();
+    },
+
+    /**
+     * 프레임을 활성화한다.
+     * @param {string} activatableId 활성화 대상 프레임 아이디
+     */
+    activate: function (activatableId) {
+      _activated = _id === activatableId;
+    },
+
+    /**
+     * 프레임이 활성화되어 있는가?
+     */
+    isActivated: function () {
+      return _activated;
+    }
+  };
+
 });
